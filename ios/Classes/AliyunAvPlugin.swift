e64f1270efbafc35320a8ba28b190fc4562b1ff4
Andy <smallsevenk@vip.qq.com>
2025-08-01 15:45:40 +0800

    ♻️ update: 代码优化

--- ios/Classes/AliyunAvPlugin.swift
+++ ios/Classes/AliyunAvPlugin.swift
@@ -1,21 +1,39 @@
+
 import Flutter
 import AUIAICall
 import UIKit
 
 public class AliyunAvPlugin: NSObject, FlutterPlugin {
-  private static var channel: FlutterMethodChannel?
-
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "aliyun_av_plugin", binaryMessenger: registrar.messenger())
-    AliyunAvPlugin.channel = channel
-    let instance = AliyunAvPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    AUIAICallManager.defaultManager.startCall(callArgs: call.arguments as! Dictionary<String, Any>) {
+    private static var channel: FlutterMethodChannel?
+    private var callManager = AUIAICallManager.defaultManager
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(
+            name: "aliyun_av_plugin",
+            binaryMessenger: registrar.messenger()
+        )
+        let instance = AliyunAvPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        self.channel = channel
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments as? [String: Any] else {
+            result(FlutterError(
+                code: "INVALID_ARGUMENTS",
+                message: "Arguments must be a dictionary",
+                details: nil
+            ))
+            return
+        }
+        
+        callManager.startCall(callArgs: args) { [weak self] in
+            self?.invokeSubtitleUpdate()
+        }
+        result(true)
+    }
+    
+    private func invokeSubtitleUpdate() {
         AliyunAvPlugin.channel?.invokeMethod("onSubtitleUpdate", arguments: nil)
     }
-    result(true)
-  }
 }
